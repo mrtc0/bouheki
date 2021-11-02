931bfcb5072c2f7dc964ea3a06080619130b94bf
Kohei Morita <mrtc0@ssrf.in>
2021-11-02 23:35:40 +0900

    format
    Signed-off-by: Kohei Morita <mrtc0@ssrf.in>

--- pkg/bpf/c/restricted-network.bpf.c
+++ pkg/bpf/c/restricted-network.bpf.c
@@ -19,7 +19,8 @@ BPF_HASH(deny_uids, struct deny_uid_key, u32, 256);
 BPF_HASH(allowed_gids, struct allowed_gid_key, u32, 256);
 BPF_HASH(deny_gids, struct deny_gid_key, u32, 256);
 
-struct {
+struct
+{
 	__uint(type, BPF_MAP_TYPE_LPM_TRIE);
 	__uint(max_entries, 256);
 	__type(key, struct ip4_trie_key);
@@ -27,7 +28,8 @@ struct {
 	__uint(map_flags, BPF_F_NO_PREALLOC);
 } denylist SEC(".maps");
 
-struct {
+struct
+{
 	__uint(type, BPF_MAP_TYPE_LPM_TRIE);
 	__uint(max_entries, 256);
 	__type(key, struct ip4_trie_key);
@@ -35,7 +37,8 @@ struct {
 	__uint(map_flags, BPF_F_NO_PREALLOC);
 } allowlist SEC(".maps");
 
-static inline void report_ip4_block(void *ctx, u64 cg, enum action action, enum network_op op, struct socket *sock, const struct sockaddr_in *daddr) {
+static inline void report_ip4_block(void *ctx, u64 cg, enum action action, enum network_op op, struct socket *sock, const struct sockaddr_in *daddr)
+{
 	struct audit_event_blocked_ipv4 ev;
 
 	struct task_struct *current_task;
@@ -50,7 +53,7 @@ static inline void report_ip4_block(void *ctx, u64 cg, enum action action, enum 
 	bpf_core_read(&ev.hdr.nodename, sizeof(ev.hdr.nodename), &uts_ns->name.nodename);
 
 	ev.hdr.cgroup = cg;
-	ev.hdr.pid = (u32) (bpf_get_current_pid_tgid() >> 32);
+	ev.hdr.pid = (u32)(bpf_get_current_pid_tgid() >> 32);
 	ev.hdr.type = BLOCKED_IPV4;
 	bpf_get_current_comm(&ev.hdr.task, sizeof(ev.hdr.task));
 
@@ -65,7 +68,8 @@ static inline void report_ip4_block(void *ctx, u64 cg, enum action action, enum 
 
 // TODO: lsm/send_msg
 SEC("lsm/socket_connect")
-int BPF_PROG(socket_connect, struct socket *sock, struct sockaddr *address, int addrlen) {
+int BPF_PROG(socket_connect, struct socket *sock, struct sockaddr *address, int addrlen)
+{
 	int allow_connect = -EPERM;
 	int allow_command = -EPERM;
 	int allow_uid = -EPERM;
@@ -73,16 +77,15 @@ int BPF_PROG(socket_connect, struct socket *sock, struct sockaddr *address, int 
 
 	// TODO: support IPv6
 	if (address->sa_family != AF_INET)
-    		return 0;
-	
+		return 0;
+
 	u64 cg = bpf_get_current_cgroup_id();
 
-	struct sockaddr_in *inet_addr = (struct sockaddr_in*)address;
+	struct sockaddr_in *inet_addr = (struct sockaddr_in *)address;
 
 	struct ip4_trie_key key = {
-    		.prefixlen = 32,
-    		.addr = inet_addr->sin_addr
-	};
+			.prefixlen = 32,
+			.addr = inet_addr->sin_addr};
 
 	struct allowed_command_key allowed_command;
 	struct deny_command_key deny_command;
@@ -103,80 +106,97 @@ int BPF_PROG(socket_connect, struct socket *sock, struct sockaddr *address, int 
 
 	struct bouheki_config *c = (struct bouheki_config *)bpf_map_lookup_elem(&b_config, &index);
 
-  // Redundant by BPF constraints...
+	// Redundant by BPF constraints...
 	int has_allow_command = 0;
 	int has_allow_uid = 0;
 	int has_allow_gid = 0;
 
-	if (c && c->has_allow_command) {
+	if (c && c->has_allow_command)
+	{
 		has_allow_command = c->has_allow_command;
 	}
-	if (c && c->has_allow_uid) {
+	if (c && c->has_allow_uid)
+	{
 		has_allow_uid = c->has_allow_uid;
 	}
 
-
-	if (c && c->target == TARGET_CONTAINER) {
-		if (!is_container()) {
+	if (c && c->target == TARGET_CONTAINER)
+	{
+		if (!is_container())
+		{
 			return 0;
 		}
 	}
 
-	if (bpf_map_lookup_elem(&allowlist, &key)) {
+	if (bpf_map_lookup_elem(&allowlist, &key))
+	{
 		allow_connect = 0;
 	}
 
-	if (bpf_map_lookup_elem(&allowed_uids, &allowed_uid) || has_allow_uid == 0) {
+	if (bpf_map_lookup_elem(&allowed_uids, &allowed_uid) || has_allow_uid == 0)
+	{
 		allow_uid = 0;
 	}
 
-	if (bpf_map_lookup_elem(&allowed_gids, &allowed_gid) || has_allow_gid == 0) {
+	if (bpf_map_lookup_elem(&allowed_gids, &allowed_gid) || has_allow_gid == 0)
+	{
 		allow_gid = 0;
 	}
 
-	if (bpf_map_lookup_elem(&allowed_commands, &allowed_command) || has_allow_command == 0) {
+	if (bpf_map_lookup_elem(&allowed_commands, &allowed_command) || has_allow_command == 0)
+	{
 		allow_command = 0;
 	}
 
-	if (bpf_map_lookup_elem(&deny_commands, &deny_command)) {
+	if (bpf_map_lookup_elem(&deny_commands, &deny_command))
+	{
 		allow_command = -EPERM;
 	}
 
-	if (bpf_map_lookup_elem(&deny_uids, &deny_uid)) {
+	if (bpf_map_lookup_elem(&deny_uids, &deny_uid))
+	{
 		allow_uid = -EPERM;
 	}
 
-	if (bpf_map_lookup_elem(&deny_gids, &deny_gid)) {
+	if (bpf_map_lookup_elem(&deny_gids, &deny_gid))
+	{
 		allow_gid = -EPERM;
 	}
 
-	if (bpf_map_lookup_elem(&denylist, &key)) {
+	if (bpf_map_lookup_elem(&denylist, &key))
+	{
 		allow_connect = -EPERM;
 	}
 
-	if (bpf_map_lookup_elem(&denylist, &key) && bpf_map_lookup_elem(&allowed_commands, &allowed_command)) {
+	if (bpf_map_lookup_elem(&denylist, &key) && bpf_map_lookup_elem(&allowed_commands, &allowed_command))
+	{
 		allow_connect = 0;
 	}
 
-	if (bpf_map_lookup_elem(&denylist, &key) && bpf_map_lookup_elem(&allowed_uids, &allowed_uid)) {
+	if (bpf_map_lookup_elem(&denylist, &key) && bpf_map_lookup_elem(&allowed_uids, &allowed_uid))
+	{
 		allow_connect = 0;
 	}
 
-	if (bpf_map_lookup_elem(&denylist, &key) && bpf_map_lookup_elem(&allowed_gids, &allowed_gid)) {
+	if (bpf_map_lookup_elem(&denylist, &key) && bpf_map_lookup_elem(&allowed_gids, &allowed_gid))
+	{
 		allow_connect = 0;
 	}
 
 	int can_access = -EPERM;
-	if (allow_connect == 0 && allow_uid == 0 && allow_gid == 0 && allow_command == 0) {
+	if (allow_connect == 0 && allow_uid == 0 && allow_gid == 0 && allow_command == 0)
+	{
 		can_access = 0;
 	}
 
-	if (can_access != 0 && c && c->mode == MODE_BLOCK) {
-		report_ip4_block((void*) ctx, cg, ACTION_BLOCK, OP_CONNECT, sock, inet_addr);
+	if (can_access != 0 && c && c->mode == MODE_BLOCK)
+	{
+		report_ip4_block((void *)ctx, cg, ACTION_BLOCK, OP_CONNECT, sock, inet_addr);
 	}
 
-	if (c && c->mode == MODE_MONITOR) {
-		report_ip4_block((void*) ctx, cg, ACTION_MONITOR, OP_CONNECT, sock, inet_addr);
+	if (c && c->mode == MODE_MONITOR)
+	{
+		report_ip4_block((void *)ctx, cg, ACTION_MONITOR, OP_CONNECT, sock, inet_addr);
 		return 0;
 	}
 
