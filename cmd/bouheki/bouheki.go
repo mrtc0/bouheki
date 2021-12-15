12995d36331eb3dee583eb81aac667c483517dd9
Kohei Morita <mrtc0@ssrf.in>
2021-12-15 17:42:11 +0900

    do not ignore...
    Signed-off-by: Kohei Morita <mrtc0@ssrf.in>

---
+++ cmd/bouheki/bouheki.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+        "os"
+
+        "github.com/mrtc0/bouheki/pkg/commands"
+        log "github.com/sirupsen/logrus"
+)
+
+var (
+        version = "dev"
+)
+
+func main() {
+        app := commands.NewApp(version)
+        err := app.Run(os.Args)
+        if err != nil {
+                log.Fatal(err)
+        }
+}
