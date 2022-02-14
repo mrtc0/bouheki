cbc524486017d9a2cc03e3ccc497617b50b9611f
Kohei Morita <mrtc0@ssrf.in>
2022-02-14 23:57:13 +0900

    style: format
    Signed-off-by: Kohei Morita <mrtc0@ssrf.in>

--- pkg/bpf/c/common.h
+++ pkg/bpf/c/common.h
@@ -6,130 +6,151 @@
 
 #define ALLOW_ACCESS 0
 
-#define AF_INET  2
-#define AUDIT_EVENTS_RING_SIZE  (4*4096)
+#define AF_INET 2
+#define AUDIT_EVENTS_RING_SIZE (4 * 4096)
 #define TASK_COMM_LEN 16
 #define NEW_UTS_LEN 64
-#define BPF_RING_BUF(name, size) \
-    struct { \
-        __uint(type, BPF_MAP_TYPE_RINGBUF); \
-        __uint(max_entries, size); \
-    } name SEC(".maps")
+#define BPF_RING_BUF(name, size)        \
+  struct                                \
+  {                                     \
+    __uint(type, BPF_MAP_TYPE_RINGBUF); \
+    __uint(max_entries, size);          \
+  } name SEC(".maps")
 
-enum mode {
-	MODE_MONITOR,
-	MODE_BLOCK
+enum mode
+{
+  MODE_MONITOR,
+  MODE_BLOCK
 };
 
-enum target {
-	TARGET_HOST,
-	TARGET_CONTAINER
+enum target
+{
+  TARGET_HOST,
+  TARGET_CONTAINER
 };
 
 #define BPF_HASH(name, key_type, val_type, size) \
-    struct { \
-        __uint(type, BPF_MAP_TYPE_HASH); \
-        __uint(max_entries, size); \
-        __type(key, key_type); \
-        __type(value, val_type); \
-    } name SEC(".maps")
+  struct                                         \
+  {                                              \
+    __uint(type, BPF_MAP_TYPE_HASH);             \
+    __uint(max_entries, size);                   \
+    __type(key, key_type);                       \
+    __type(value, val_type);                     \
+  } name SEC(".maps")
 
-enum network_op {
-	OP_CONNECT,
-	OP_SENDMSG
+enum network_op
+{
+  OP_CONNECT,
+  OP_SENDMSG
 };
 
-enum action {
-	ACTION_MONITOR,
-	ACTION_BLOCK
+enum action
+{
+  ACTION_MONITOR,
+  ACTION_BLOCK
 };
 
-enum audit_event_type {
-	BLOCKED_IPV4,
-	BLOCKED_IPV6
+enum audit_event_type
+{
+  BLOCKED_IPV4,
+  BLOCKED_IPV6
 };
 
-struct audit_event_header {
-	u64 cgroup;
-	u32 pid;
-	enum audit_event_type type;
-	char nodename[NEW_UTS_LEN + 1];
-	char task[TASK_COMM_LEN];
-	char parent_task[TASK_COMM_LEN];
+struct audit_event_header
+{
+  u64 cgroup;
+  u32 pid;
+  enum audit_event_type type;
+  char nodename[NEW_UTS_LEN + 1];
+  char task[TASK_COMM_LEN];
+  char parent_task[TASK_COMM_LEN];
 };
 
-struct audit_event_blocked_ipv4 {
-	struct audit_event_header hdr;
-	struct in_addr src;
-	struct in_addr dst;
-	u16 dport;
-	u8 operation;
-	u8 action;
-	u8 sock_type;
+struct audit_event_blocked_ipv4
+{
+  struct audit_event_header hdr;
+  struct in_addr src;
+  struct in_addr dst;
+  u16 dport;
+  u8 operation;
+  u8 action;
+  u8 sock_type;
 };
 
-struct bouheki_config {
-	enum mode mode;
-	enum target target;
-	int has_allow_command;
-	int has_allow_uid;
+struct bouheki_config
+{
+  enum mode mode;
+  enum target target;
+  int has_allow_command;
+  int has_allow_uid;
 };
 
-struct ip4_trie_key {
-	u32 prefixlen;
-	struct in_addr addr;
+struct ip4_trie_key
+{
+  u32 prefixlen;
+  struct in_addr addr;
 };
 
-struct allowed_command_key {
-	char comm[TASK_COMM_LEN];
+struct allowed_command_key
+{
+  char comm[TASK_COMM_LEN];
 };
 
-struct deny_command_key {
-	char comm[TASK_COMM_LEN];
+struct deny_command_key
+{
+  char comm[TASK_COMM_LEN];
 };
 
-struct allowed_uid_key {
-	u32 uid;
+struct allowed_uid_key
+{
+  u32 uid;
 };
 
-struct deny_uid_key {
-	u32 uid;
+struct deny_uid_key
+{
+  u32 uid;
 };
 
-struct allowed_gid_key {
-	u32 gid;
+struct allowed_gid_key
+{
+  u32 gid;
 };
 
-struct deny_gid_key {
-	u32 gid;
+struct deny_gid_key
+{
+  u32 gid;
 };
 
-static inline struct in_addr src_addr4(const struct socket *sock) {
-	struct in_addr addr;
-	__builtin_memset(&addr, 0, sizeof(addr));
+static inline struct in_addr src_addr4(const struct socket *sock)
+{
+  struct in_addr addr;
+  __builtin_memset(&addr, 0, sizeof(addr));
 
-	addr.s_addr = BPF_CORE_READ(sock, sk, __sk_common.skc_rcv_saddr);
-	return addr;
+  addr.s_addr = BPF_CORE_READ(sock, sk, __sk_common.skc_rcv_saddr);
+  return addr;
 }
 
-static inline int _is_host_mntns() {
+static inline int _is_host_mntns()
+{
   struct task_struct *current_task;
-	struct nsproxy *nsproxy;
+  struct nsproxy *nsproxy;
   struct mnt_namespace *mnt_ns;
-	unsigned int inum;
+  unsigned int inum;
 
-	current_task = (struct task_struct *)bpf_get_current_task();
+  current_task = (struct task_struct *)bpf_get_current_task();
 
-	bpf_core_read(&nsproxy, sizeof(nsproxy), &current_task->nsproxy);
-	bpf_core_read(&mnt_ns, sizeof(mnt_ns), &nsproxy->mnt_ns);
-	bpf_core_read(&inum, sizeof(inum), &mnt_ns->ns.inum);
-	if (inum == 0xF0000000) {
-		return true;
-	}
+  bpf_core_read(&nsproxy, sizeof(nsproxy), &current_task->nsproxy);
+  bpf_core_read(&mnt_ns, sizeof(mnt_ns), &nsproxy->mnt_ns);
+  bpf_core_read(&inum, sizeof(inum), &mnt_ns->ns.inum);
+  if (inum == 0xF0000000)
+  {
+    return true;
+  }
 
-	return false;
+  return false;
 }
 
-static inline int is_container() {
-	return !_is_host_mntns();
+static inline int is_container()
+{
+  return !_is_host_mntns();
 }
--- pkg/bpf/c/restricted-network.bpf.c
+++ pkg/bpf/c/restricted-network.bpf.c
@@ -21,198 +21,198 @@ BPF_HASH(denied_gid_list, struct deny_gid_key, u32, 256);
 
 struct
 {
-	__uint(type, BPF_MAP_TYPE_LPM_TRIE);
-	__uint(max_entries, 256);
-	__type(key, struct ip4_trie_key);
-	__type(value, char);
-	__uint(map_flags, BPF_F_NO_PREALLOC);
+  __uint(type, BPF_MAP_TYPE_LPM_TRIE);
+  __uint(max_entries, 256);
+  __type(key, struct ip4_trie_key);
+  __type(value, char);
+  __uint(map_flags, BPF_F_NO_PREALLOC);
 } denied_cidr_list SEC(".maps");
 
 struct
 {
-	__uint(type, BPF_MAP_TYPE_LPM_TRIE);
-	__uint(max_entries, 256);
-	__type(key, struct ip4_trie_key);
-	__type(value, char);
-	__uint(map_flags, BPF_F_NO_PREALLOC);
+  __uint(type, BPF_MAP_TYPE_LPM_TRIE);
+  __uint(max_entries, 256);
+  __type(key, struct ip4_trie_key);
+  __type(value, char);
+  __uint(map_flags, BPF_F_NO_PREALLOC);
 } allowed_cidr_list SEC(".maps");
 
 static inline void report_ip4_block(void *ctx, u64 cg, enum action action, enum network_op op, struct socket *sock, const struct sockaddr_in *daddr)
 {
-	struct audit_event_blocked_ipv4 ev;
+  struct audit_event_blocked_ipv4 ev;
 
-	struct task_struct *current_task;
-	struct uts_namespace *uts_ns;
-	struct nsproxy *nsproxy;
-	current_task = (struct task_struct *)bpf_get_current_task();
+  struct task_struct *current_task;
+  struct uts_namespace *uts_ns;
+  struct nsproxy *nsproxy;
+  current_task = (struct task_struct *)bpf_get_current_task();
 
-	bpf_core_read(&nsproxy, sizeof(nsproxy), &current_task->nsproxy);
-	bpf_core_read(&uts_ns, sizeof(uts_ns), &nsproxy->uts_ns);
+  bpf_core_read(&nsproxy, sizeof(nsproxy), &current_task->nsproxy);
+  bpf_core_read(&uts_ns, sizeof(uts_ns), &nsproxy->uts_ns);
 
-	__builtin_memset(&ev, 0, sizeof(ev));
-	bpf_core_read(&ev.hdr.nodename, sizeof(ev.hdr.nodename), &uts_ns->name.nodename);
+  __builtin_memset(&ev, 0, sizeof(ev));
+  bpf_core_read(&ev.hdr.nodename, sizeof(ev.hdr.nodename), &uts_ns->name.nodename);
 
-	ev.hdr.cgroup = cg;
-	ev.hdr.pid = (u32)(bpf_get_current_pid_tgid() >> 32);
-	ev.hdr.type = BLOCKED_IPV4;
-	bpf_get_current_comm(&ev.hdr.task, sizeof(ev.hdr.task));
+  ev.hdr.cgroup = cg;
+  ev.hdr.pid = (u32)(bpf_get_current_pid_tgid() >> 32);
+  ev.hdr.type = BLOCKED_IPV4;
+  bpf_get_current_comm(&ev.hdr.task, sizeof(ev.hdr.task));
 
-	struct task_struct *parent_task = BPF_CORE_READ(current_task, real_parent);
-	bpf_probe_read_kernel_str(&ev.hdr.parent_task, sizeof(ev.hdr.parent_task), &parent_task->comm);
+  struct task_struct *parent_task = BPF_CORE_READ(current_task, real_parent);
+  bpf_probe_read_kernel_str(&ev.hdr.parent_task, sizeof(ev.hdr.parent_task), &parent_task->comm);
 
-	ev.dport = __builtin_bswap16(daddr->sin_port);
-	ev.src = src_addr4(sock);
-	ev.dst = BPF_CORE_READ(daddr, sin_addr);
-	ev.operation = (u8)op;
-	ev.action = (u8)action;
-	ev.sock_type = (u8)sock->type;
+  ev.dport = __builtin_bswap16(daddr->sin_port);
+  ev.src = src_addr4(sock);
+  ev.dst = BPF_CORE_READ(daddr, sin_addr);
+  ev.operation = (u8)op;
+  ev.action = (u8)action;
+  ev.sock_type = (u8)sock->type;
 
-	bpf_ringbuf_output(&audit_events, &ev, sizeof(ev), 0);
+  bpf_ringbuf_output(&audit_events, &ev, sizeof(ev), 0);
 }
 
 static inline bool is_destination_port_zero(struct sockaddr_in *inet_addr)
 {
-	return __builtin_bswap16(inet_addr->sin_port) == 0;
+  return __builtin_bswap16(inet_addr->sin_port) == 0;
 }
 
 // TODO: lsm/send_msg
 SEC("lsm/socket_connect")
 int BPF_PROG(socket_connect, struct socket *sock, struct sockaddr *address, int addrlen)
 {
-	int allow_connect = -EPERM;
-	int allow_command = -EPERM;
-	int allow_uid = -EPERM;
-	int allow_gid = -EPERM;
+  int allow_connect = -EPERM;
+  int allow_command = -EPERM;
+  int allow_uid = -EPERM;
+  int allow_gid = -EPERM;
 
-	// TODO: support IPv6
-	if (address->sa_family != AF_INET)
-		return 0;
+  // TODO: support IPv6
+  if (address->sa_family != AF_INET)
+    return 0;
 
-	u64 cg = bpf_get_current_cgroup_id();
+  u64 cg = bpf_get_current_cgroup_id();
 
-	struct sockaddr_in *inet_addr = (struct sockaddr_in *)address;
+  struct sockaddr_in *inet_addr = (struct sockaddr_in *)address;
 
-	if (is_destination_port_zero(inet_addr))
-	{
-		return 0;
-	}
+  if (is_destination_port_zero(inet_addr))
+  {
+    return 0;
+  }
 
-	struct ip4_trie_key key = {
-		.prefixlen = 32,
-		.addr = inet_addr->sin_addr};
+  struct ip4_trie_key key = {
+      .prefixlen = 32,
+      .addr = inet_addr->sin_addr};
 
-	struct allowed_command_key allowed_command;
-	struct deny_command_key deny_command;
-	struct allowed_uid_key allowed_uid;
-	struct deny_uid_key deny_uid;
-	struct allowed_gid_key allowed_gid;
-	struct deny_gid_key deny_gid;
+  struct allowed_command_key allowed_command;
+  struct deny_command_key deny_command;
+  struct allowed_uid_key allowed_uid;
+  struct deny_uid_key deny_uid;
+  struct allowed_gid_key allowed_gid;
+  struct deny_gid_key deny_gid;
 
-	bpf_get_current_comm(&allowed_command.comm, sizeof(allowed_command.comm));
-	bpf_get_current_comm(&deny_command.comm, sizeof(deny_command.comm));
+  bpf_get_current_comm(&allowed_command.comm, sizeof(allowed_command.comm));
+  bpf_get_current_comm(&deny_command.comm, sizeof(deny_command.comm));
 
-	allowed_uid.uid = (unsigned)(bpf_get_current_uid_gid() & 0xffffffff);
-	deny_uid.uid = (unsigned)(bpf_get_current_uid_gid() & 0xffffffff);
-	allowed_gid.gid = (unsigned)(bpf_get_current_uid_gid() >> 32);
-	deny_gid.gid = (unsigned)(bpf_get_current_uid_gid() >> 32);
+  allowed_uid.uid = (unsigned)(bpf_get_current_uid_gid() & 0xffffffff);
+  deny_uid.uid = (unsigned)(bpf_get_current_uid_gid() & 0xffffffff);
+  allowed_gid.gid = (unsigned)(bpf_get_current_uid_gid() >> 32);
+  deny_gid.gid = (unsigned)(bpf_get_current_uid_gid() >> 32);
 
-	u32 index = 0;
+  u32 index = 0;
 
-	struct bouheki_config *c = (struct bouheki_config *)bpf_map_lookup_elem(&bouheki_config, &index);
+  struct bouheki_config *c = (struct bouheki_config *)bpf_map_lookup_elem(&bouheki_config, &index);
 
-	// Redundant by BPF constraints...
-	int has_allow_command = 0;
-	int has_allow_uid = 0;
-	int has_allow_gid = 0;
+  // Redundant by BPF constraints...
+  int has_allow_command = 0;
+  int has_allow_uid = 0;
+  int has_allow_gid = 0;
 
-	if (c && c->has_allow_command)
-	{
-		has_allow_command = c->has_allow_command;
-	}
-	if (c && c->has_allow_uid)
-	{
-		has_allow_uid = c->has_allow_uid;
-	}
+  if (c && c->has_allow_command)
+  {
+    has_allow_command = c->has_allow_command;
+  }
+  if (c && c->has_allow_uid)
+  {
+    has_allow_uid = c->has_allow_uid;
+  }
 
-	if (c && c->target == TARGET_CONTAINER)
-	{
-		if (!is_container())
-		{
-			return 0;
-		}
-	}
+  if (c && c->target == TARGET_CONTAINER)
+  {
+    if (!is_container())
+    {
+      return 0;
+    }
+  }
 
-	if (bpf_map_lookup_elem(&allowed_cidr_list, &key))
-	{
-		allow_connect = 0;
-	}
+  if (bpf_map_lookup_elem(&allowed_cidr_list, &key))
+  {
+    allow_connect = 0;
+  }
 
-	if (bpf_map_lookup_elem(&allowed_uid_list, &allowed_uid) || has_allow_uid == 0)
-	{
-		allow_uid = 0;
-	}
+  if (bpf_map_lookup_elem(&allowed_uid_list, &allowed_uid) || has_allow_uid == 0)
+  {
+    allow_uid = 0;
+  }
 
-	if (bpf_map_lookup_elem(&allowed_gid_list, &allowed_gid) || has_allow_gid == 0)
-	{
-		allow_gid = 0;
-	}
+  if (bpf_map_lookup_elem(&allowed_gid_list, &allowed_gid) || has_allow_gid == 0)
+  {
+    allow_gid = 0;
+  }
 
-	if (bpf_map_lookup_elem(&allowed_command_list, &allowed_command) || has_allow_command == 0)
-	{
-		allow_command = 0;
-	}
+  if (bpf_map_lookup_elem(&allowed_command_list, &allowed_command) || has_allow_command == 0)
+  {
+    allow_command = 0;
+  }
 
-	if (bpf_map_lookup_elem(&denied_command_list, &deny_command))
-	{
-		allow_command = -EPERM;
-	}
+  if (bpf_map_lookup_elem(&denied_command_list, &deny_command))
+  {
+    allow_command = -EPERM;
+  }
 
-	if (bpf_map_lookup_elem(&denied_uid_list, &deny_uid))
-	{
-		allow_uid = -EPERM;
-	}
+  if (bpf_map_lookup_elem(&denied_uid_list, &deny_uid))
+  {
+    allow_uid = -EPERM;
+  }
 
-	if (bpf_map_lookup_elem(&denied_gid_list, &deny_gid))
-	{
-		allow_gid = -EPERM;
-	}
+  if (bpf_map_lookup_elem(&denied_gid_list, &deny_gid))
+  {
+    allow_gid = -EPERM;
+  }
 
-	if (bpf_map_lookup_elem(&denied_cidr_list, &key))
-	{
-		allow_connect = -EPERM;
-	}
+  if (bpf_map_lookup_elem(&denied_cidr_list, &key))
+  {
+    allow_connect = -EPERM;
+  }
 
-	if (bpf_map_lookup_elem(&denied_cidr_list, &key) && bpf_map_lookup_elem(&allowed_command_list, &allowed_command))
-	{
-		allow_connect = 0;
-	}
+  if (bpf_map_lookup_elem(&denied_cidr_list, &key) && bpf_map_lookup_elem(&allowed_command_list, &allowed_command))
+  {
+    allow_connect = 0;
+  }
 
-	if (bpf_map_lookup_elem(&denied_cidr_list, &key) && bpf_map_lookup_elem(&allowed_uid_list, &allowed_uid))
-	{
-		allow_connect = 0;
-	}
+  if (bpf_map_lookup_elem(&denied_cidr_list, &key) && bpf_map_lookup_elem(&allowed_uid_list, &allowed_uid))
+  {
+    allow_connect = 0;
+  }
 
-	if (bpf_map_lookup_elem(&denied_cidr_list, &key) && bpf_map_lookup_elem(&allowed_gid_list, &allowed_gid))
-	{
-		allow_connect = 0;
-	}
+  if (bpf_map_lookup_elem(&denied_cidr_list, &key) && bpf_map_lookup_elem(&allowed_gid_list, &allowed_gid))
+  {
+    allow_connect = 0;
+  }
 
-	int can_access = -EPERM;
-	if (allow_connect == 0 && allow_uid == 0 && allow_gid == 0 && allow_command == 0)
-	{
-		can_access = 0;
-	}
+  int can_access = -EPERM;
+  if (allow_connect == 0 && allow_uid == 0 && allow_gid == 0 && allow_command == 0)
+  {
+    can_access = 0;
+  }
 
-	if (can_access != 0 && c && c->mode == MODE_BLOCK)
-	{
-		report_ip4_block((void *)ctx, cg, ACTION_BLOCK, OP_CONNECT, sock, inet_addr);
-	}
+  if (can_access != 0 && c && c->mode == MODE_BLOCK)
+  {
+    report_ip4_block((void *)ctx, cg, ACTION_BLOCK, OP_CONNECT, sock, inet_addr);
+  }
 
-	if (c && c->mode == MODE_MONITOR)
-	{
-		report_ip4_block((void *)ctx, cg, ACTION_MONITOR, OP_CONNECT, sock, inet_addr);
-		return 0;
-	}
+  if (c && c->mode == MODE_MONITOR)
+  {
+    report_ip4_block((void *)ctx, cg, ACTION_MONITOR, OP_CONNECT, sock, inet_addr);
+    return 0;
+  }
 
-	return can_access;
+  return can_access;
 }
