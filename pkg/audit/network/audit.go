a33726041e90c9adbcac02250867861fced308ac
Kohei Morita <mrtc0@ssrf.in>
2022-04-14 18:12:24 +0900

    feat: Remove the responsibility to modify resolv.conf
    Signed-off-by: Kohei Morita <mrtc0@ssrf.in>

--- pkg/audit/network/audit.go
+++ pkg/audit/network/audit.go
@@ -5,7 +5,6 @@ import (
 	"context"
 	"encoding/binary"
 	"io"
-	"io/ioutil"
 	"sync"
 
 	"github.com/miekg/dns"
@@ -132,10 +131,6 @@ func RunAudit(ctx context.Context, wg *sync.WaitGroup, conf *config.Config) erro
 	if err != nil {
 		return err
 	}
-	oldResolvConf, err := ioutil.ReadFile("/etc/resolv.conf")
-	if err != nil {
-		return err
-	}
 
 	mgr := Manager{
 		mod:    mod,
@@ -153,9 +148,13 @@ func RunAudit(ctx context.Context, wg *sync.WaitGroup, conf *config.Config) erro
 
 	if mgr.config.EnableDNSProxy() {
 		go func() {
-			err := mgr.StartDNSServer()
+			err := mgr.StartDNSServer(hostDNSBindAddress)
 			if err != nil {
-				updateResolvConf("/etc/resolv.conf", oldResolvConf)
+			}
+		}()
+		go func() {
+			err := mgr.StartDNSServer(dockerDNSBindAddress)
+			if err != nil {
 			}
 		}()
 	} else {
@@ -189,7 +188,6 @@ func RunAudit(ctx context.Context, wg *sync.WaitGroup, conf *config.Config) erro
 
 	<-ctx.Done()
 	mgr.Close()
-	updateResolvConf("/etc/resolv.conf", oldResolvConf)
 	log.Info("Terminated the network audit.")
 
 	return nil
